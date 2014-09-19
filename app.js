e9f62e2c08d099fa4345bd1a66febe0bd1979926
Ken Fehling <ken@androidideas.org>
2014-09-19 01:07:42 -0400

    Fixed Heroku port

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
  */
 
 /*jshint strict: true */
-/*global require, console */
+/*global require, console, process */
 
 var server = require('./server');
 var constants = require('drum-circle-library/constants');
 
-server.listen(constants.DEFAULT_API_PORT, function() {
+var port = process.env.PORT || constants.DEFAULT_API_PORT;
+server.listen(port, function() {
     "use strict";
     console.log('%s listening at %s', server.name, server.url);
 });
