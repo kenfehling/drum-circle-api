cbd939230d0d5e9e63a401681f76de14232b2ba0
Ken Fehling <ken@androidideas.org>
2014-09-19 01:20:18 -0400

    Added favicon.ico

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 /*jshint strict: true */
 /*global require, console, process */
 
+var fs = require('fs');
 var server = require('./server');
 var constants = require('drum-circle-library/constants');
 
@@ -12,4 +13,22 @@ var port = process.env.PORT || constants.DEFAULT_API_PORT;
 server.listen(port, function() {
     "use strict";
     console.log('%s listening at %s', server.name, server.url);
+});
+
+server.get('/favicon.ico', function(req, res, next) {
+    "use strict";
+    fs.readFile('./public/favicon.ico', function(err, file) {
+        if (err) {
+            res.send(500);
+        }
+        else {
+            res.send({
+                code: 200,
+                noEnd: true
+            });
+            res.write(file);
+            res.end();
+        }
+        return next();
+    });
 });
